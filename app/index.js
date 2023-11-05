aef61d660b56975ddf9b2c974a7a43c0f39f0297
Kurasad <akowaidev@gmail.com>
2023-11-05 06:37:50 -0600

    feat: a half working app

---
+++ app/index.js
@@ -0,0 +1,4 @@
+import { WebView } from 'react-native-webview';
+
+export default _ => <WebView source={{ uri: 'http://10.0.2.2:3000/' }} />;
+// export default _ => <WebView source={{ uri: 'https://yieldly.netlify.app' }} />;
