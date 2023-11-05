5b66c432143d564ed2aab3c68db028d4684edf8f
Kurasad <akowaidev@gmail.com>
2023-11-05 07:00:27 -0600

    fix: unused vars

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -5,7 +5,7 @@ import 'font-awesome/css/font-awesome.min.css';
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Home = () => {
-  
+
   // Sample data for miles left and max capacity
   const milesLeft = 250; // Replace with actual data
   const maxCapacity = 500; // Replace with actual data
@@ -21,101 +21,101 @@ const Home = () => {
       elem.addEventListener('click', (e) => {
         if (working || !['batteryW', 'hvacW', 'brakesW'].includes(e.target.id)) return;
         working = true;
-          const id = e.target.id;
-          window.location.href = `${"http://localhost:3000/"}${id}`;
+        const id = e.target.id;
+        window.location.href = `${"http://localhost:3000/"}${id}`;
       });
     })
   }
 
-  const {logout, isAuthenticated} = useAuth0();
+  const { isAuthenticated } = useAuth0();
 
-  if(isAuthenticated){
+  if (isAuthenticated) {
     window.location.href = `http://localhost:3000/Logout`;
-  }else{
-      return (<>
-        <div className="home-screen">
-          <Header />
-          <div className="fuel-info">
-            <div className="fuel-icon">
-            </div>
-        <b>{milesLeft} mi</b>
+  } else {
+    return (<>
+      <div className="home-screen">
+        <Header />
+        <div className="fuel-info">
+          <div className="fuel-icon">
+          </div>
+          <b>{milesLeft} mi</b>
+        </div>
+        <div className="percentage-bar">
+          <div className="filler" style={{ width: `${percentage}%` }}></div>
+        </div>
+        <div className="car-image" />
+        <div className="container">
+          <div className="line"></div>
+          <div className="rounded-rectangle">
+            {'Suggested Maintenance'}
+            <p>&#x1F893;</p>
+          </div>
+        </div>
+
+        <section>
+          <div className='tab red' id='batteryW'>
+            <div className='icon' id="battery"></div>
+            <div className='content'>Battery</div>
+          </div>
+
+          <div className='tab yellow' id='hvacW'>
+            <div className='icon' id="hvac"></div>
+            <div className='content'>HVAC</div>
+          </div>
+
+          <div className='tab' id='airbagW'>
+            <div className='icon' id="airbag"></div>
+            <div className='content'>Airbag</div>
+          </div>
+
+          <div className='tab' id='brakesW'>
+            <div className='icon' id="brake"></div>
+            <div className='content'>Brakes</div>
+          </div>
+
+          <div className='tab' id='coolantW'>
+            <div className='icon' id="coolant"></div>
+            <div className='content'>Coolant</div>
+          </div>
+
+          <div className='tab' id='dashW'>
+            <div className='icon' id="dash"></div>
+            <div className='content'>Dashboard</div>
+          </div>
+
+          <div className='tab' id='lightsW'>
+            <div className='icon' id="lights"></div>
+            <div className='content'>Headlights</div>
+          </div>
+
+          <div className='tab' id='motorW'>
+            <div className='icon' id="motor"></div>
+            <div className='content'>Motor</div>
+          </div>
+
+          <div className='tab' id='steeringW'>
+            <div className='icon' id="steering"></div>
+            <div className='content'>Steering</div>
+          </div>
+
+          <div className='tab' id='transmissionW'>
+            <div className='icon' id="transmission"></div>
+            <div className='content'>Transmission</div>
+          </div>
+
+          <div className='tab' id='wheelW'>
+            <div className='icon' id="wheel"></div>
+            <div className='content'>Wheel</div>
+          </div>
+
+          <div className='tab' id='wiperW'>
+            <div className='icon' id="wiper"></div>
+            <div className='content'>Wipers</div>
+          </div>
+        </section>
       </div>
-      <div className="percentage-bar">
-        <div className="filler" style={{ width: `${percentage}%` }}></div>
-      </div>
-      <div className="car-image" />
-      <div className="container">
-        <div className="line"></div>
-        <div className="rounded-rectangle">
-          {'Suggested Maintenance'}
-          <p>&#x1F893;</p>
-        </div>
-      </div>
-
-      <section>
-        <div className='tab red' id='batteryW'>
-          <div className='icon' id="battery"></div>
-          <div className='content'>Battery</div>
-        </div>
-
-        <div className='tab yellow' id='hvacW'>
-          <div className='icon' id="hvac"></div>
-          <div className='content'>HVAC</div>
-        </div>
-
-        <div className='tab' id='airbagW'>
-          <div className='icon' id="airbag"></div>
-          <div className='content'>Airbag</div>
-        </div>
-
-        <div className='tab' id='brakesW'>
-          <div className='icon' id="brake"></div>
-          <div className='content'>Brakes</div>
-        </div>
-
-        <div className='tab' id='coolantW'>
-          <div className='icon' id="coolant"></div>
-          <div className='content'>Coolant</div>
-        </div>
-
-        <div className='tab' id='dashW'>
-          <div className='icon' id="dash"></div>
-          <div className='content'>Dashboard</div>
-        </div>
-
-        <div className='tab' id='lightsW'>
-          <div className='icon' id="lights"></div>
-          <div className='content'>Headlights</div>
-        </div>
-
-        <div className='tab' id='motorW'>
-          <div className='icon' id="motor"></div>
-          <div className='content'>Motor</div>
-        </div>
-
-        <div className='tab' id='steeringW'>
-          <div className='icon' id="steering"></div>
-          <div className='content'>Steering</div>
-        </div>
-
-        <div className='tab' id='transmissionW'>
-          <div className='icon' id="transmission"></div>
-          <div className='content'>Transmission</div>
-        </div>
-
-        <div className='tab' id='wheelW'>
-          <div className='icon' id="wheel"></div>
-          <div className='content'>Wheel</div>
-        </div>
-
-        <div className='tab' id='wiperW'>
-          <div className='icon' id="wiper"></div>
-          <div className='content'>Wipers</div>
-        </div>
-      </section>
-    </div>
-  </>);
-};
+    </>);
+  };
 }
 
 export default Home;
